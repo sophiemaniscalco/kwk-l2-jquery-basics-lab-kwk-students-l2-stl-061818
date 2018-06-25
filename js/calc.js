bace000c5184a7df44e31d4424ffd976c0a8ab2d
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-25 14:00:07 +0000

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,39 @@
 //add your code here
+
+$(document).ready(function(){
+  
+  doMath();
+});
+
+var num1=$('#number1')
+var num2=$('#number2')
+var operator=$('#operation')
+
+function doMath() {
+  $('#equals').on('click', function(){
+  num1=Number.parseFloat(num1.val());
+  num2=parseFloat(num2.val());
+  operator=operator.val();
+  result(num1, num2, operator)
+  })
+}
+
+function result(num1, num2, operator){
+  if (operator == '+') {
+    $('#result').html(num1 + num2)
+    return(num1 + num2)  
+    
+  }
+  
+  else if (operator == "-") {
+    $("#result").html(num1 - num2)
+  }
+  
+  else if (operator == "*") {
+    $("#result").html(num1 * num2)
+  }
+  else if (operator == "/") {
+    $("#result").html(num1 / num2)
+    
+  }
+}
